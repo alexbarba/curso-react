45e7bc6103f9fabd3076863c3a45966653344bfc
Alexis Barba <alexbarba.2278@gmail.com>
2019-01-16 18:47:45 -0700

    Manejo de errores a traves del componente HandleError

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,11 +4,13 @@ import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
-
+import '../../error/containers/handle-error';
+import HandleError from '../../error/containers/handle-error';
 
 class Home extends Component {
   state = {
     modalVisible: false,
+    handleError: false,
   }
   handleOpenModal = () => {
     this.setState({
@@ -20,26 +22,36 @@ class Home extends Component {
       modalVisible: false,
     })
   }
+  componentDidCatch(error, info){
+    this.setState({
+      handleError: true,
+    })
+  }
 
   render(){
+    if(this.state.handleError){
+      return <p>OHH hay un error :(</p>
+    }
     return(
-      <HomeLayout>
-        <Related />
-        <Categories
-          categories={this.props.data.categories}
-          handleOpenModal={this.handleOpenModal}
-        />
-        {
-          this.state.modalVisible &&
-          <ModalContainer>
-            <Modal
-              handleClick={this.handleCloseModal}
-            >
-              <h1>Esto es un portal</h1>
-            </Modal>
-          </ModalContainer>
-        }
-      </HomeLayout>
+      <HandleError>
+        <HomeLayout>
+          <Related />
+          <Categories
+            categories={this.props.data.categories}
+            handleOpenModal={this.handleOpenModal}
+          />
+          {
+            this.state.modalVisible &&
+            <ModalContainer>
+              <Modal
+                handleClick={this.handleCloseModal}
+              >
+                <h1>Esto es un portal</h1>
+              </Modal>
+            </ModalContainer>
+          }
+        </HomeLayout>
+      </HandleError>
     )
   }
 }
