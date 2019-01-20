a77face0f36f8d287a3813b5c4e59963cf43a542
Alexis Barba <alexbarba.2278@gmail.com>
2019-01-20 15:32:34 -0700

    Referencias a Eventos de HTML y Formularios

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './search.css';
 
-const Search = () => (
-  <form className='Search'>
+const Search = (props) => (
+  <form 
+    className='Search'
+    onSubmit={props.handleSubmit}
+  >
     <input
       type="text"
       className='Search-input'
       placeholder='Buscar'
+      ref={props.setRef}
+      onChange={props.handleChange}
+      value={props.value}
     />
   </form>
 )
