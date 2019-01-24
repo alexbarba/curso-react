21c60cc5faecd7c06382167fe5cc1ac22e28eac6
Alexis Barba <alexbarba.2278@gmail.com>
2019-01-24 12:41:20 -0700

    Agregando lógica al botón de Play y Pausa

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import './video.css'
 
 class Video extends Component{
-  render() {
+  togglePlay() {
+    if (this.props.pause) {
+      this.video.play()
+    } else {
+      this.video.pause()
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pause !== this.props.pause) {
+      this.togglePlay();
+    }
+  }
+  setRef = element => {
+    this.video = element;
+  }
+  render() { 
     return(
       <div className='Video'>
         <video 
           src={this.props.src}
           autoPlay={this.props.autoplay}
+          ref={this.setRef}
         />
       </div>
     )
